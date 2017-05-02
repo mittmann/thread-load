a6f441023c034cfefa672f8b77a29ce3acf3eba3
Arthur Krause <arthur.krause@hotmail.com>
2017-05-02 20:09:12 -0300

    /proc/PID/stat reading and outputing virtual memory size

--- connect.c
+++ connect.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #if defined(_OPENMP)
 	#include <omp.h>
@@ -53,6 +54,7 @@ static int veci = 0;
 static uint32_t order = 0;
 static int started = 0;
 static uint64_t s_jiffies = 0;
+procinfo *pinfo;
 #define WRAPPER_LABEL(LABEL)                     LABEL
 #define REAL_LABEL(LABEL)                        libtload_connect_real_##LABEL
 #define DECLARE_WRAPPER(LABEL, RETTYPE, ...)     static RETTYPE (*REAL_LABEL(LABEL)) (__VA_ARGS__) = NULL
@@ -262,6 +264,9 @@ int WRAPPER_LABEL(pthread_create) (pthread_t *a_thread, const pthread_attr_t *at
 	if(!started)
     {  
 		started=1;
+		pinfo = (procinfo*)malloc(sizeof(procinfo));
+	    get_proc_info(getpid(), pinfo);	
+		dprintf("statistics of process vsize %d\n", pinfo->vsize);
 		s_jiffies = get_thread_load(thread->kernel_pid, thread->kernel_tid);
 		#ifdef LIBTLOAD_SUPPORT_PAPI
 			libtload_papi_init();
@@ -340,5 +345,87 @@ static void __attribute__((destructor)) triggered_on_app_end ()
 			stat_printf(t->order_id, "jiffies", load);
 		}
 	}
+}
+
+int get_proc_info(pid_t pid, procinfo * pinfo)
+{
+  char szFileName [_POSIX_PATH_MAX],
+    szStatStr [2048],
+    *s, *t;
+  FILE *fp;
+  struct stat st;
+  
+  if (NULL == pinfo) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  sprintf (szFileName, "/proc/%u/stat", (unsigned) pid);
+  
+  if (-1 == access (szFileName, R_OK)) {
+    return (pinfo->pid = -1);
+  } /** if **/
+
+  if (-1 != stat (szFileName, &st)) {
+  	pinfo->euid = st.st_uid;
+  	pinfo->egid = st.st_gid;
+  } else {
+  	pinfo->euid = pinfo->egid = -1;
+  }
+  
+  
+  if ((fp = fopen (szFileName, "r")) == NULL) {
+    return (pinfo->pid = -1);
+  } /** IF_NULL **/
+  
+  if ((s = fgets (szStatStr, 2048, fp)) == NULL) {
+    fclose (fp);
+    return (pinfo->pid = -1);
+  }
+
+  /** pid **/
+  sscanf (szStatStr, "%u", &(pinfo->pid));
+  s = strchr (szStatStr, '(') + 1;
+  t = strchr (szStatStr, ')');
+  strncpy (pinfo->exName, s, t - s);
+  pinfo->exName [t - s] = '\0';
+  
+  sscanf (t + 2, "%c %d %d %d %d %d %u %u %u %u %u %d %d %d %d %d %d %u %u %d %u %u %u %u %u %u %u %u %d %d %d %d %u",
+	  /*       1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33*/
+	  &(pinfo->state),
+	  &(pinfo->ppid),
+	  &(pinfo->pgrp),
+	  &(pinfo->session),
+	  &(pinfo->tty),
+	  &(pinfo->tpgid),
+	  &(pinfo->flags),
+	  &(pinfo->minflt),
+	  &(pinfo->cminflt),
+	  &(pinfo->majflt),
+	  &(pinfo->cmajflt),
+	  &(pinfo->utime),
+	  &(pinfo->stime),
+	  &(pinfo->cutime),
+	  &(pinfo->cstime),
+	  &(pinfo->counter),
+	  &(pinfo->priority),
+	  &(pinfo->timeout),
+	  &(pinfo->itrealvalue),
+	  &(pinfo->starttime),
+	  &(pinfo->vsize),
+	  &(pinfo->rss),
+	  &(pinfo->rlim),
+	  &(pinfo->startcode),
+	  &(pinfo->endcode),
+	  &(pinfo->startstack),
+	  &(pinfo->kstkesp),
+	  &(pinfo->kstkeip),
+	  &(pinfo->signal),
+	  &(pinfo->blocked),
+	  &(pinfo->sigignore),
+	  &(pinfo->sigcatch),
+	  &(pinfo->wchan));
+  fclose (fp);
+  return 0;
 }
 
